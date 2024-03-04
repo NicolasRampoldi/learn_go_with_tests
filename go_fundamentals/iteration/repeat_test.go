0236ee0c7202206843dc880550eb4ff61a0bb741
NicolasRampoldi <58613770+NicolasRampoldi@users.noreply.github.com>
2024-03-04 18:20:30 -0300

    feat: add Iteration module from Go Fundamentals

---
+++ go_fundamentals/iteration/repeat_test.go
@@ -0,0 +1,36 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func Repeat(character string, times int) string {
+	var repeated string
+	for i := 0; i < times; i++ {
+		repeated += character
+	}
+
+	return repeated
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 5)
+	fmt.Println(repeated)
+	// Output: aaaaa
+}
